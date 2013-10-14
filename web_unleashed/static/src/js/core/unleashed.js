1235bcc460041085de0159f58397793851bfced1
Michel Meyer <michel@zazabe.fr>
2013-10-14 15:35:34 +0700

    use Marionette.Application and Module for unleashed core

--- web_unleashed/static/src/js/core/unleashed.js
+++ web_unleashed/static/src/js/core/unleashed.js
@@ -1,12 +1,11 @@
 (function(openerp){
     //FIXME: current BackboneJS version is 0.9.2 on OpenERP, to get the latest features from Backbone 1.0, a local version is used there under Backbone
     //       can be removed when BackboneJs will be updated on OpenERP...
-    var LatestBackbone = Backbone.noConflict();
-    var LatestUnderscore = _.noConflict();      
+    var LatestBackbone = Backbone.noConflict(),
+        LatestUnderscore = _.noConflict(),
+        Marionette = LatestBackbone.Marionette;
     
-    // will get the web object defined by OpenERP
-    var web = {};
-    
+
     // AttributeHolder access
     var AttributeAccess = {
         add: function(namespace, name, obj){
@@ -24,68 +23,87 @@
         get: function(namespace, name){
             var obj = this.Class && this.Class[namespace] && this.Class[namespace][name] ?  this.Class[namespace][name] : null; 
             if(!obj){
-                throw new Error('"' + namespace + '.' + name + '" can not be found in module "' + this.name + '"');
+                throw new Error('"' + namespace + '.' + name + '" can not be found in module "' + this.moduleName + '"');
             }
             return obj;
         },
     };
-      
-      
-      
-    var getModule = function(module_name, base_module){
-        var module = null;
-        if(this[module_name]){
-            module = this[module_name];
+       
+    _.extend(Marionette.Module.prototype, {
+        /*
+         * wrap a module initializer and pass useful object to the callback
+         */
+        ready: function(callback){
+            this.addInitializer(function(options){
+                var instance = options.instance;
+                callback.apply(this, [instance, this, LatestUnderscore, LatestBackbone, this.app.module('web_unleashed')])
+            });
+        },
+        
+        get: function(namespace, name){
+            // handle no namespace definition
+            if(!name){
+                name = namespace;
+                namespace = 'Misc';
+            }
+            return AttributeAccess.get.apply(this, [namespace, name]);
+        },
+        
+        set: function(namespace, name, obj){
+            // handle no namespace definition
+            if(!obj){
+                obj = name; 
+                name = namespace;
+                namespace = 'Misc';
+            }
+            return AttributeAccess.add.apply(this, [namespace, name, obj]);
+        },
+        
+        // helpers
+        
+        models: function(name, obj){
+            return AttributeAccess.add.apply(this, ['Models', name, obj]);
+        },
+        
+        collections: function(name, obj){
+            return AttributeAccess.add.apply(this, ['Collections', name, obj]);
+        },
+        
+        views: function(name, obj){
+            return AttributeAccess.add.apply(this, ['Views', name, obj]);
+        },
+        
+        utils: function(name, obj){
+            return AttributeAccess.add.apply(this, ['Utils', name, obj]);
         }
-        else {
-            var def = $.Deferred();
+    });   
+    
+
+    // add some specific method to Marionette.Module 
+    var moduleCreate = Marionette.Module.create;
+    _.extend(Marionette.Module, {
+
+        // manage Class, to be able to split the architecture 
+        // in many files and keep all organized in on object
+        Class: {},
+
+        /*
+         * override the create module to autostart it when OpenERP is ready
+         */
+        create: function(app, moduleNames, moduleDefinition){
+            var module = moduleCreate.apply(this, arguments);
             
-            module =  _.extend({
-                // manage Class, to be able to split the architecture in many files and keep all organized in 
-                // on object
-                Class: {},
-            
-                name: module_name,
-                
-                models: function(name, obj){
-                    return AttributeAccess.add.apply(this, ['Models', name, obj]);
-                },
-                
-                collections: function(name, obj){
-                    return AttributeAccess.add.apply(this, ['Collections', name, obj]);
-                },
-                
-                views: function(name, obj){
-                    return AttributeAccess.add.apply(this, ['Views', name, obj]);
-                },
-                
-                utils: function(name, obj){
-                    return AttributeAccess.add.apply(this, ['Utils', name, obj]);
-                },
-                
-                ready: function(call){
-                    def.done(call);
-                },
-                
-                isReady: function(){
-                    return def.state() === 'resolved';
-                }, 
-                // provide OpenERP instance.web object if available
-                web: function(){ return web; }
-            }, LatestBackbone.Events);
-            
-            openerp[module_name] = function(instance){
-                def.resolveWith(module, [instance, module, LatestUnderscore, LatestBackbone, base_module || null]);
+            openerp[moduleNames] = function(instance){
+                module.start({
+                    instance: instance
+                });
             };
             
-            this[module_name] = module;
+            return module; 
         }
-        
-        return module;
-    };
-
-
-
+    });  
+       
+       
     /*
      * Initialize a  Unleashed JS namespace, with Backbone MVC architecture and JSON-RPC connection support.
      */
@@ -93,28 +111,36 @@
     // null until the web_unleashed is initialized...
     var sync = null;
     
-    var unleashed = openerp.unleashed = {
-        module: function(module_name, call){
-            
-            var base_module = getModule.apply(this, ['web_unleashed']);
-            var module = getModule.apply(this, [module_name, base_module]);
-            
-            if(typeof call == 'function'){
-                call.apply(openerp, [module, LatestUnderscore, LatestBackbone, base_module]);
-            }
-            
-            return module;
-        },
+    var Unleashed = openerp.unleashed = new Marionette.Application();
+    
+    Unleashed.addInitializer(function(options){
+        var module = this.module,
+            app = this;
         
-        sync: function(method, model, options){
+        // override the module creation function to have all modules in startWithParent false mode
+        this.module = function(name, callback){
+            var call = function(){
+                this.startWithParent = false;
+                if(callback){
+                    callback.apply(this, [this, LatestUnderscore, LatestBackbone, this.app.module('web_unleashed')])
+                }
+            };
+            return module.apply(app, [name, call]);
+        };
+        
+        // sync method used by Backbone Models to access to OpenERP data by the JSON-RPC API
+        this.sync = function(method, model, options){
             if(!sync){
                 throw new Error('Model ' + model + ' can not be sync (' + method + ') yet, waiting for OpenERP module initalization...');
             }
             else {
                 return sync.apply(this,[ method, model, options ]);
             }
-        }  
-    };
+        };    
+    });
+    
+    // start the app
+    Unleashed.start();
     
     
     
@@ -122,16 +148,14 @@
      * prepare OpenERP translation methods on Backbone, only available when the base module is initialized 
      */
    
-    
-
-    unleashed.module('web_unleashed').ready(function(instance, _, Backbone){
+    Unleashed.module('web_unleashed').ready(function(instance, base, _, Backbone){
         // link instance.web object and provide it to each modules
         web = instance.web; 
         
         // setup the connection with JSON-RPC API for Backbone
         
         var connection = instance.web.Model;
-        var Connector = unleashed.module('web_unleashed').utils('Connector');
+        var Connector = base.utils('Connector');
         
         debug = connection;
         /*
@@ -160,10 +184,7 @@
                 console_debug.apply(console, arguments);
             }
         };
-         
-        
     });
-    
     
     
 })(openerp)
