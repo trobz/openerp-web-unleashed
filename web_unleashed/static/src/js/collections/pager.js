7867ca1ff0dc83d9ad80076bfaea6638fded61e5
Michel Meyer <michel@zazabe.fr>
2013-10-17 13:34:36 +0700

    clean up libs, add jsdoc comment to all objects, fix mixins issues

--- web_unleashed/static/src/js/collections/pager.js
+++ web_unleashed/static/src/js/collections/pager.js
@@ -1,34 +1,56 @@
 openerp.unleashed.module('web_unleashed', function(base, _, Backbone){
     
     var BaseCollection = base.collections('BaseCollection'),
-        _super = BaseCollection.prototype;
+        _superCollection = BaseCollection.prototype;
     
-    var PagerController = base.utils('Pager'),
+    var PagerController = base.controllers('Pager'),
         _superPager = PagerController.prototype;
     
-    var PagerCollection = PagerController.extend(_super);
     
-    var Pager = PagerCollection.extend({
-        model_name: null,
+    /*
+     * @class
+     * @module      web_unleashed
+     * @name        PagerCollection
+     * @classdesc   Paginated collection, use JSON-RPC API to navigate between pages 
+     * @mixes       BaseCollection, PagerController
+     * 
+     * @author Michel Meyer <michel[at]zazabe.com>
+     */
+   
+    /*
+     * inherit from BaseCollection and extends PagerController and BaseCollection prototypes
+     */
+    MixedPagerCollection = function(){
+        BaseCollection.apply(this, arguments);
+    };
     
+    MixedPagerCollection.prototype = _.extend(
+        Object.create(_superPager),
+        Object.create(_superCollection)
+    );
+    
+    MixedPagerCollection.prototype.constructor = MixedPagerCollection;
+    MixedPagerCollection.extend = BaseCollection.extend;    
+
+    var PagerCollection = MixedPagerCollection.extend({
+    
+        /*
+         * - initialize all extended objects
+         */
         initialize: function(data, options){
-            _super.initialize.apply(this, arguments);
+            _superCollection.initialize.apply(this, arguments);
             _superPager.initialize.apply(this, [options]);
         },  
-            
-        update: function(search){
-            return this.fetch(this.search());
-        },
-        
-        count: function(search){
-            return this.sync('count', this, this.search());
-        },
-        
+       
+        /*
+         * get the search query, depending of the current page
+         * @see PagerController.search
+         */    
         search: function(){
             return _superPager.search.apply(this, arguments);
         }
     });
 
-    base.collections('Pager', Pager);
+    base.collections('Pager', PagerCollection);
 
 });
